a15dfde8726d3e32394648e4b309e533aada69b2
Mike <tradewcs@gmail.com>
2025-11-10 15:20:41 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
-# write your code here
+def copy_file(command: str) -> None:
+    args = command.split(" ")
+    if len(args) != 3 or args[0] != "cp":
+        print("Usage: cp <source> <destination>")
+        return
+
+    source = args[1]
+    destination = args[2]
+
+    try:
+        with open(source, "rb") as src_file, \
+             open(destination, "wb") as dest_file:
+            dest_file.write(src_file.read())
+    except FileNotFoundError:
+        print(f"Error: File '{source}' not found.")
+    except IOError as e:
+        print(f"Error: {e}")
