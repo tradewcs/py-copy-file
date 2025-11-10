473053deae51990714905bf5e60b82cb7d0845c3
Mike <tradewcs@gmail.com>
2025-11-10 15:22:22 +0200

    fixed source == destination case

--- app/main.py
+++ app/main.py
@@ -7,6 +7,9 @@ def copy_file(command: str) -> None:
     source = args[1]
     destination = args[2]
 
+    if source == destination:
+        return
+
     try:
         with open(source, "rb") as src_file, \
              open(destination, "wb") as dest_file:
